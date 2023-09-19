900cfbdc7d763c2fa2720421efcc4642c3286726
whoiscyberial <whoiscyberial@gmail.com>
2023-09-19 23:44:23 +0500

    adapted for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Quiz } from "./components/Quiz";
 
 export default function Home() {
   return (
-    <main className="flex text-center min-h-screen flex-col items-center justify-center m-auto w-[40%] 2xl:w-[25%]">
+    <main className="flex text-center min-h-screen flex-col items-center justify-center m-auto w-full p-8 lg:w-[40%] 2xl:w-[25%]">
       <Quiz></Quiz>
     </main>
   );
