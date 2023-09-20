d59d6304b25cdc04dd7f9c9a651676ae45cf6a47
whoiscyberial <whoiscyberial@gmail.com>
2023-09-20 14:19:19 +0500

    confetti

--- app/components/Result.tsx
+++ app/components/Result.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ChoiceButton } from "./ChoiceButton";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 type ResultProps = {
   result: string;
@@ -7,8 +9,11 @@ type ResultProps = {
 };
 
 export const Result = ({ result, setChoices }: ResultProps) => {
+  const { width, height } = useWindowSize();
+
   return (
     <div className="w-full">
+      <Confetti width={width} height={height} numberOfPieces={100} />
       <h2 className="text-sm font-medium  text-slate-300">Ваш результат</h2>
       <h1 className="text-5xl font-bold mt-3 mb-16 text-slate-50">{result}</h1>
       <ChoiceButton setChoices={setChoices} answer="" choices="">
